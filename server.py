cafa05c19f3fd64ee8d1a3920810142be77ce30b
Dmitry Golovchenko <golovchen@gmail.com>
2022-12-10 20:13:45 +0500

    async telegram.getFullUser

--- server.py
+++ server.py
@@ -1,27 +1,45 @@
-from concurrent import futures
+import traceback
 
 import grpc
-from telethon import TelegramClient
+from telethon import TelegramClient, functions
 import os
+import asyncio
 
 import protos_pb2_grpc
-
-
-class TelegramServer(protos_pb2_grpc.TelegramClient):
-    pass
-
+from protos_pb2 import User, GetUserRequest
 
 api_id = int(os.environ['API_ID'])
 api_hash = os.environ['API_HASH']
 client = TelegramClient("session", api_id, api_hash)
 client.start()
 
-client.run_until_disconnected()
+class TelegramServer(protos_pb2_grpc.TelegramClientServicer):
+    async def GetUser(self, request: GetUserRequest, context: grpc.aio.ServicerContext) -> User:
+        try:
+            print("get user " + str(request))
+            result = await client(functions.users.GetFullUserRequest(
+                id=request.user_id
+            ))
+            print(result)
+            result = result.users[0]
+            return User(first_name=result.first_name, last_name=result.last_name, username=result.username)
+        except:
+            print("exception")
+            traceback.print_exc()
 
-port = '50051'
-server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-protos_pb2_grpc.add_TelegramClientServicer_to_server(TelegramServer(), server)
-server.add_insecure_port('[::]:' + port)
-server.start()
-print("Server started, listening on port " + port)
-server.wait_for_termination()
+
+
+async def serve() -> None:
+    try:
+        server = grpc.aio.server()
+        protos_pb2_grpc.add_TelegramClientServicer_to_server(TelegramServer(), server)
+        listen_addr = '[::]:50051'
+        server.add_insecure_port(listen_addr)
+        print("Starting server on %s", listen_addr)
+        await server.start()
+        await server.wait_for_termination()
+    except:
+        traceback.print_exc()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(serve())
