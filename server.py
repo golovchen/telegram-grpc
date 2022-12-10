2b7a938d55729ad09641ef75cad3cd3578dcd94b
Dmitry Golovchenko <golovchen@gmail.com>
2022-12-10 18:35:28 +0500

    empty server

--- server.py
+++ server.py
@@ -1,8 +1,27 @@
+from concurrent import futures
+
+import grpc
 from telethon import TelegramClient
 import os
+
+import protos_pb2_grpc
+
+
+class TelegramServer(protos_pb2_grpc.TelegramClient):
+    pass
+
 
 api_id = int(os.environ['API_ID'])
 api_hash = os.environ['API_HASH']
 client = TelegramClient("session", api_id, api_hash)
 client.start()
+
 client.run_until_disconnected()
+
+port = '50051'
+server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+protos_pb2_grpc.add_TelegramClientServicer_to_server(TelegramServer(), server)
+server.add_insecure_port('[::]:' + port)
+server.start()
+print("Server started, listening on port " + port)
+server.wait_for_termination()
